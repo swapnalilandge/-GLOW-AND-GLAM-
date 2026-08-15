c5beab5a17e401a4eacba29aea7e2aeb569f2898
swapnali <swapnalilandge17@gmail.com>
2026-08-15 18:18:02 +0530

    Use process.env.PORT for Render deployment

--- server.js
+++ server.js
@@ -210,6 +210,8 @@ app.get("/orders/:email", (req, res) => {
         res.json(result);
     });
 });
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
